0dc7a1412ea4ec8803af1795f6b9ac8c59c4e685
barbyns <barbyns95@gmail.com>
2025-04-01 17:09:15 +0200

    update all

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,49 @@
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Carousel, ListGroup } from "react-bootstrap"
+import pastasciutte from '../data/menu.json'
 
 const Home = function (){
     return (
         <Container>
             <Row className="justify-content-center">
                 <Col xs={12} md={8} lg={6}>
-                <h1>Epistaurant</h1>
                 <h2>Le migliori pastasciutte del web!</h2>
-
                 </Col>
             </Row>
+            <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                <Carousel>
+                    {pastasciutte.map((pasta) => {
+                            return(
+                                <Carousel.Item key={pasta.id}>
+                                <img
+                    src={pasta.image}
+                    className="w-100"
+                    alt={'immagine di ' + pasta.name}
+                  />
+                  <Carousel.Caption>
+                    <h3>{pasta.name}</h3>
+                    <p>{pasta.description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                         )
+                        })}
+                    </Carousel>
+                    </Col>
+                    </Row>
+                    <Row className="justify-content-center">
+                    <Col xs={12} md={8} lg={6} className="text-center">
+                    <ListGroup>
+                        {
+                            pastasciutte[0].comments.map((recensione)=>{
+                                return (
+                                <ListGroup.Item key={recensione.id}>{recensione.comment}- {recensione.rating}/5</ListGroup.Item>
+                            )
+                            })
+                        }
+                       
+                    </ListGroup>
+                </Col>
+                </Row>
         </Container>
     )
 }
